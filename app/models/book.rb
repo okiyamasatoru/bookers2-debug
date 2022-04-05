0447d54e5fee163f259199f082c93815a4e2dfc2
EC2 Default User <ec2-user@ip-172-31-39-210.ap-northeast-1.compute.internal>
2022-04-05 05:56:26 +0000

    +search

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,4 +7,17 @@ class Book < ApplicationRecord
   def favorited_by?(user)
       favorites.where(user_id: user.id).exists?
   end
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 end
