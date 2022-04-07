7b80286fdd92fd687487b227f358383c9530a340
EC2 Default User <sqt01v@gmail.com>
2022-04-07 12:41:27 +0000

    messege

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following_user, through: :follower, source: :followed
   has_many :follower_user, through: :followed, source: :follower
+  has_many :user_rooms, dependent: :destroy
+ has_many :chats, dependent: :destroy
   # ユーザーをフォローする
   def follow(user_id)
    follower.create(followed_id: user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'chats/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
   get "search" => "searches#search"
+  get 'chat/:id', to: 'chats#show', as: 'chat'
+  resources :chats, only: [:create]
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
